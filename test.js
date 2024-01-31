fb26e9469fcaca5ea9185e4aa8a6cbc373a15594
AndrewOhwoka <andrew.ohwoka@keyin.com>
2024-01-31 16:40:37 -0330

    firstCommit

---
+++ test.js
@@ -0,0 +1,52 @@
+// function args(a) {
+//     console.log(arguments.length, a, arguments [0]); }
+
+// // args(1, 2, 3);
+// args()
+
+
+// function add(a, b) {
+//     return a + b;
+// }
+// console.log(add(2, 3));
+
+
+// function add(a, b) { console.log(a + b)}
+// add(1, 2);
+
+// function area(radius)
+// {
+//     return parseFloat(radius)*Math.PI * radius * radius;
+// }
+// console.log(area("2"));
+
+
+// let reversed3 = reverse_word(arr[2]);
+
+// if (arr[0] !== reversed1) {
+//     return false;
+// }
+
+// if (arr[1] !== reversed2) {
+//     return false;
+// }
+
+// if (arr[2] !== reversed3) {
+//     return false;
+// }
+// return true;
+// }
+
+// if (arr[0] !== reversed1) {
+//     return false;
+// }
+
+// if (arr[1] !== reversed2) {
+//     return false;
+// }
+
+// if (arr[2] !== reversed3) {
+//     return false;
+// }
+// return true;
+// }
